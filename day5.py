de7bc2cbd36d6800ca3dac922c826b16f2727e60
manvithaS12 <4mu22cs091@mysururoyal.org>
2026-02-07 18:14:37 +0530

    Added day5 encapsulation task

---
+++ day5.py
@@ -0,0 +1,66 @@
+class InstagramAccount:
+    def __init__(self, account_name, password):
+        self.account_name = account_name
+        self._private_reels = []
+        self.__archived_reels = []
+        self.__password = password
+
+    def add_private_reel(self, reel_name):
+        self._private_reels.append(reel_name)
+        print(f"Private reel '{reel_name}' added.")
+
+    def display_private_reels(self, is_follower):
+        if is_follower:
+            print("Private Reels:")
+            for reel in self._private_reels:
+                print("-", reel)
+        else:
+            print("Access Denied! Only followers can view private reels")
+
+    def add_archived_reel(self, reel_name):
+        self.__archived_reels.append(reel_name)
+        print(f"Archived reel '{reel_name}' added.")
+
+    def display_archived_reels(self, password):
+        if password == self.__password:
+            print("Archived Reels:")
+            for reel in self.__archived_reels:
+                print("-", reel)
+
+        else:
+            print("Access Denied! Only account holder can view archived reels")
+
+    def get_archived_reels(self, password):
+        if password == self.__password:
+            return self.__archived_reels
+        else:
+            print("Wrong Password! Cannot access archived reels")
+            return None
+
+    def set_password(self, old_password, new_password):
+        if old_password == self.__password:
+            self.__password = new_password
+            print("Password updated successfully")
+        else:
+            print("Wrong old password! Cannot update password")
+
+acc = InstagramAccount("insta", "1234")
+acc.add_private_reel("trip reel")
+acc.add_private_reel("Party reel")
+
+acc.add_archived_reel("Old Dance Video")
+acc.add_archived_reel("School Memories")
+
+acc.display_private_reels(True)
+acc.display_private_reels(False)
+
+acc.display_archived_reels("1234")
+acc.display_archived_reels("0234")
+
+data = acc.get_archived_reels("1234")
+
+if data:
+    print(data)
+
+acc.set_password("1234", "5678")
+acc.display_archived_reels("5678")
