7578a93a21d39f8e42ef1e4351b01741fa40deb5
manvithaS12 <4mu22cs091@mysururoyal.org>
2026-02-11 09:03:39 +0530

    task1 of dunder methods

---
+++ dunder/d1.py
@@ -0,0 +1,17 @@
+class Book:
+    def __init__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
+    def __str__(self):
+        return f"'{self.title}' by {self.author} costs ₹{self.price}"
+
+    def __repr__(self):
+        return f"Book(title='{self.title}', author='{self.author}', price={self.price})"
+
+book1 = Book("Python Programming", "Guido van Rossum", 499)
+book2 = Book("OOP Mastery", "James Gosling", 599)
+print(book1)
+print(book2)
+
+print([book1, book2])
